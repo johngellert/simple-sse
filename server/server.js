c0aae8eff10cae9929aa4b24d844e06551c88f78
John Gellert <gellertjm@gmail.com>
2019-09-12 16:23:59 -0500

    Updates client with messages.

--- server/server.js
+++ server/server.js
@@ -1,43 +1,67 @@
-const express = require('express');
+const express = require("express");
+const app = express(); // declare an express app
+const PORT = process.env.PORT || 5000;
+var cors = require("cors"); // allow cross-origin resource sharing
 const pool = require("./modules/pool");
 
-// declare an express app
-const app = express();
-
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/* Routes */
-app.post('/api/colors', (req, res) => {
-  const query = `INSERT INTO "colors" (color) VALUES ($1);`;
-  const { color } = req.body;
+let resultsColors = [];
+app.get("/color-events", cors(), (req, res) => {
+  // SSE Setup
+  let messageId = 0;
+  res.writeHead(200, {
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache"
+  });
 
-  pool.query(query, [color]).then(() => {
-    res.sendStatus(200);  
-  }).catch(error => {
-    console.log(`Error with POST color query:`, error);
-    res.sendStatus(500);
-  })
+  setInterval(() => {
+    res.write(`id:${messageId}\n`);
+    res.write("event:colorUpdates\n");
+    res.write(`data:${JSON.stringify(resultsColors)}\n`);
+    res.write("\n\n");
+    messageId += 1;
+  }, 1000);
+
 });
 
-app.get('/api/colors', (req, res) => {
-  const query = `SELECT * FROM "colors" ORDER BY "id" DESC;`;
-  pool.query(query).then((results) => {
-    res.send(results.rows);
-  }).catch(error => {
-    console.log(`Error with GET color query:`, error);
-    res.sendStatus(500);
-  })
-})
+const getColors = () => {
+  const query = `SELECT * FROM "colors" ORDER BY "id" DESC LIMIT 10;`;
+  console.log("getting colors");
+  pool
+    .query(query)
+    .then(results => {
+      resultsColors = [...results.rows];
+      console.log(resultsColors);
+    })
+    .catch(error => {
+      console.log(`Error with GET color query:`, error);
+    });
+};
+
+/* Routes */
+app.post("/api/colors", (req, res) => {
+  const query = `INSERT INTO "colors" (color) VALUES ($1);`;
+  const { color } = req.body;
+  pool
+    .query(query, [color])
+    .then(() => {
+      res.sendStatus(201);
+      getColors();
+    })
+    .catch(error => {
+      console.log(`Error with POST color query:`, error);
+      res.sendStatus(500);
+    });
+});
 
 // Serve static files
-app.use(express.static('build'));
-
-// App Set //
-const PORT = process.env.PORT || 5000;
+app.use(express.static("build"));
 
 /** Listen * */
 app.listen(PORT, () => {
--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,14 +1,36 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import "eventsource-polyfill";
 
 function History(props) {
+  const eventSource = new EventSource("http://localhost:5000/color-events");
+  eventSource.addEventListener("colorUpdates", e => {
+    console.log(JSON.parse(e.data));
+  });
+  //   const [history, setHistory] = useState({
+  //     colorHistory: []
+  //   });
+
+  //   eventSource.addEventListener("colorUpdates", e => {
+  //     setHistory({
+  //       colorHistory: [...JSON.parse(e.data)]
+  //     });
+  //   });
+
+  //   eventSource.addEventListener("colorUpdates", e => {
+  //       props.dispatch({ type: "SET_COLORS", payload: JSON.parse(e.data) });
+  //     });
+
   return (
     <div className="history-container">
-        <h2>Recently Added</h2>
+      <h2>Recently Added</h2>
       <ul>
         {props.state.colors.map(item => (
           <li key={item.id}>{item.color}</li>
         ))}
+        {/* {history.colorHistory.map(item => (
+          <li key={item.id}>{item.color}</li>
+        ))} */}
       </ul>
     </div>
   );
